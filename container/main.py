fbb7ebdb2cdf0c480aa1b4798ffb9296f644b620
raknatee <52844836+raknatee@users.noreply.github.com>
2024-07-23 16:16:18 +0700

    update

--- container/main.py
+++ container/main.py
@@ -6,22 +6,22 @@ app = FastAPI()
 
 @app.get("/webhook")
 def listen(request:Request):
+
+
     verify_webhook(request)
 
 
-class FacebookToken(TypedDict):
-    "hub.mode"
-    "hub.verify"
-    "hub.challenge"
 
 
-def verify_webhook(hub_mode:str, hub_verify:str, hub_challenge):
+
+
+def verify_webhook(req:Request):
     
-    calling_verify_token = req.query_params
-    
+    calling_verify_token = req.query_params.get("hub.verify_token")
+    print(calling_verify_token)
     fb_verify_token = "<Your Verification Token>"
     if calling_verify_token == fb_verify_token:
-        challenge = req.args.get("hub.challenge")
+        challenge = req.query_params.get("hub.challenge")
         return challenge
     else:
         return "incorrect"
